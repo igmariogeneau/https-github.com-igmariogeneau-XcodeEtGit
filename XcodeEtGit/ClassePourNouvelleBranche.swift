e6a13ac12186b11724b3850682bd0d6792b0e5a2
Mario Geneau <MAD@users-macbook-pro.local>
2017-06-27 11:52:40 -0400

    NB_v1

---
+++ XcodeEtGit/ClassePourNouvelleBranche.swift
@@ -0,0 +1,13 @@
+//
+//  ClassePourNouvelleBranche.swift
+//  XcodeEtGit
+//
+//  Created by Mario Geneau on 2017-06-27.
+//  Copyright © 2017 Mario Geneau. All rights reserved.
+//
+
+import Foundation
+
+class ClassePourNouvelleBranche {
+    
+}
